119c581cce745e139c07cc6dc6ae4d9ce2614216
Prati28 <sankhepratiksha3@gmail.com>
2024-07-02 16:27:15 +0530

    basic structure based on cookiecutter
    Signed-off-by: Prati28 <sankhepratiksha3@gmail.com>

---
+++ tus_storagehandler/__init__.py
@@ -0,0 +1,1 @@
+"""This package contains module for the tus_storagehandler."""
---
+++ tus_storagehandler/main.py
@@ -0,0 +1,10 @@
+"""Entry point for python_cookiecutter."""
+
+
+def main():
+  """Main entry point for tus_storagehandler."""
+  print("Hello from tus_storagehandler!")
+
+
+if __name__ == "__main__":
+  main()
